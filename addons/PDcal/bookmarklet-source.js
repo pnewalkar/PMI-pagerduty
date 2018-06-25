60cde028f56d05b5d9aaca6fa4e2e5cab9693409
pnewalkar <pnewalkar@gmail.com>
2018-06-25 13:24:05 +0100

    adding addons

---
+++ addons/PDcal/bookmarklet-source.js
@@ -0,0 +1,16 @@
+javascript: (
+	function() {
+		$('ul.cal-options-menu').each(function() {
+			var ul = $(this);
+			$(this).find('li.just-my-cal-option > a').each(function() {
+				if ( ul.find('i.icon-external-link-sign').length > 0 ) {
+					return;
+				}
+				var icalurl = $(this).attr('href').replace(/\?.*$/, '');
+				var href = 'https://pagerduty.github.io/addons/PDcal/index.html?iCalURL=' + icalurl;
+				$(ul).append('<li class="cal-subheader"><h4 class="dropdown-menu-subheader"><i class="icon icon-external-link-sign icon-fixed-width"></i>Public View</h4></li>');
+				$(ul).append('<li class="cal-option"><a class=action-link cal-link" href="' + href + '" target="_blank">Open in new tab</a>');
+			});
+		});
+	}
+)
